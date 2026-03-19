ae245d8a2eb03f858e6284875c3b8297d5e1376c
yongjae <kiyoja07@gmail.com>
2026-03-19 10:45:52 +0900

    #4.4 Ladle

--- app-dev-environment/widgets/vite.config.ts
+++ app-dev-environment/widgets/vite.config.ts
@@ -1,11 +1,16 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
+const FULL_URL = "https://server.kiyoja07.workers.dev/"; // server를 배포한 실제 URL
+
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  build: {
-    outDir: "../server/dist",
-    emptyOutDir: true,
-  },
+export default defineConfig(({ mode }) => {
+  return {
+    plugins: [react()],
+    base: mode === "production" ? FULL_URL : undefined,
+    build: {
+      outDir: "../server/dist",
+      emptyOutDir: true,
+    },
+  };
 });
