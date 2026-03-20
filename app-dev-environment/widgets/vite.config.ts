482b005f500010658bdc6f457d774535b9c4f3a5
yongjae <kiyoja07@gmail.com>
2026-03-20 10:12:31 +0900

    #4.5 Apps SDK UI

--- app-dev-environment/widgets/vite.config.ts
+++ app-dev-environment/widgets/vite.config.ts
@@ -1,4 +1,5 @@
 import { defineConfig } from "vite";
+import tailwindcss from "@tailwindcss/vite";
 import react from "@vitejs/plugin-react";
 
 const FULL_URL = "https://server.kiyoja07.workers.dev/"; // server를 배포한 실제 URL
@@ -6,7 +7,7 @@ const FULL_URL = "https://server.kiyoja07.workers.dev/"; // server를 배포한 
 // https://vite.dev/config/
 export default defineConfig(({ mode }) => {
   return {
-    plugins: [react()],
+    plugins: [react(), tailwindcss()],
     base: mode === "production" ? FULL_URL : undefined,
     build: {
       outDir: "../server/dist",
